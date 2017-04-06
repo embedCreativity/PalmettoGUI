bb62a1c3f0b874d7a2ae651582d212a822e7c947
Mark Taylor <mark@pocydon.com>
2017-04-06 00:04:30 -0400

    minimum joystick functionality stripped from evdev

--- joystick.py
+++ joystick.py
@@ -1,32 +1,89 @@
-import os
-import time
-import pygame
+#!/usr/bin/env python
 
-pygame.display.init()
-pygame.joystick.init()
-pygame.joystick.Joystick(0).init()
+from evdev import InputDevice, categorize, ecodes
 
-# Prints the joystick's name
-JoyName = pygame.joystick.Joystick(0).get_name()
-print "Name of the joystick:"
-print JoyName
+dev = InputDevice('/dev/input/event7')
 
-pygame.joystick.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+print 'Using {} as device!'.format(dev)
+
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        if event.code == ecodes.BTN_A:
+            if event.value == 1:
+                print 'BTN A down!'
+            else:
+                print 'BTN A up!'
+        elif event.code == ecodes.BTN_B:
+            if event.value == 1:
+                print 'BTN B down!'
+            else:
+                print 'BTN B up!'
+        elif event.code == ecodes.BTN_X:
+            if event.value == 1:
+                print 'BTN X down!'
+            else:
+                print 'BTN X up!'
+        elif event.code == ecodes.BTN_Y:
+            if event.value == 1:
+                print 'BTN Y down!'
+            else:
+                print 'BTN Y up!'
+        elif event.code == ecodes.BTN_TL:
+            if event.value == 1:
+                print 'BTN TL down!'
+            else:
+                print 'BTN TL up!'
+        elif event.code == ecodes.BTN_TR:
+            if event.value == 1:
+                print 'BTN TR down!'
+            else:
+                print 'BTN TR up!'
+        elif event.code == ecodes.BTN_SELECT:
+            if event.value == 1:
+                print 'BTN SELECT down!'
+            else:
+                print 'BTN SELECT up!'
+        elif event.code == ecodes.BTN_START:
+            if event.value == 1:
+                print 'BTN START down!'
+            else:
+                print 'BTN START up!'
+        elif event.code == ecodes.BTN_MODE:
+            if event.value == 1:
+                print 'BTN MODE down!'
+            else:
+                print 'BTN MODE up!'
+        elif event.code == ecodes.BTN_THUMBL:
+            if event.value == 1:
+                print 'BTN THUMBL down!'
+            else:
+                print 'BTN THUMBL up!'
+        elif event.code == ecodes.BTN_THUMBR:
+            if event.value == 1:
+                print 'BTN THUMBR down!'
+            else:
+                print 'BTN THUMBR up!'
+
+    elif event.type == ecodes.EV_ABS:
+        if event.code == ecodes.ABS_X:
+            print 'ABS_X: {}'.format(event.value)
+        elif event.code == ecodes.ABS_Y:
+            print 'ABS_Y: {}'.format(event.value)
+        elif event.code == ecodes.ABS_Z:
+            print 'ABS_Z: {}'.format(event.value)
+        elif event.code == ecodes.ABS_RX:
+            print 'ABS_RX: {}'.format(event.value)
+        elif event.code == ecodes.ABS_RY:
+            print 'ABS_RY: {}'.format(event.value)
+        elif event.code == ecodes.ABS_RZ:
+            print 'ABS_RZ: {}'.format(event.value)
+        elif event.code == ecodes.ABS_HAT0X:
+            print 'ABS_HAT0X: {}'.format(event.value)
+        elif event.code == ecodes.ABS_HAT0Y:
+            print 'ABS_HAT0Y: {}'.format(event.value)
 
 
-# Gets the number of axes
-JoyAx = pygame.joystick.Joystick(0).get_numaxes()
-print "Number of axis:"
 
 
-# Prints the values for axis0
-while True:
-    pygame.event.pump()
-    os.system('clear')
-    foo = ''
-    for i in range(joystick.get_numaxes()):
-        foo += '{:.2f} '.format(pygame.joystick.Joystick(0).get_axis(i))
-    print foo
-    time.sleep(0.01)
+
+
